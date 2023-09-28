4059170418ac049244dc1fbd3767f40f52fb47e7
Ali Mirzad <97289679+AliMirzad@users.noreply.github.com>
2023-09-29 00:17:01 +0330

    add digit algo added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(_6_AddDigit.addDigit(3214));
     }
 }
---
+++ src/_6_AddDigit.java
@@ -0,0 +1,18 @@
+public class _6_AddDigit {
+    public static int addDigit(int num) {
+        while (num / 10 != 0) {
+            int sum = 0;
+            while (num > 0) {
+                int rem = num % 10;
+                sum += rem;
+                num /= 10;
+            }
+            if (sum / 10 == 0) {
+                return sum;
+            } else {
+                num = sum;
+            }
+        }
+        return num;
+    }
+}
