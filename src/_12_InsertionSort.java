cb780e5fa13760ce5cfca89dd245fcf783174a72
Ali Mirzad <97289679+AliMirzad@users.noreply.github.com>
2023-10-05 03:23:56 +0330

    insertion sort algo added

--- src/_12_InsertionSort.java
+++ src/_12_InsertionSort.java
@@ -7,7 +7,6 @@ public class _12_InsertionSort {
         for (i = 1; i < n; i++) {
             temp = a[i];
             j = i - 1;
-
             while (j >= 0 && temp <= a[j])  /* Move the elements greater than temp to one position ahead from their current position*/ {
                 a[j + 1] = a[j];
                 j = j - 1;
