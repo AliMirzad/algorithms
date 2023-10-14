c548a715e04b666ed210875f6498845b3bb18f60
Ali Mirzad <97289679+AliMirzad@users.noreply.github.com>
2023-10-14 22:56:29 +0330

    some little change

--- src/_16_InsertionSort.java
+++ src/_16_InsertionSort.java
@@ -1,20 +1,20 @@
 // Java program for implementation of Insertion Sort
 public class _16_InsertionSort {
     /*Function to sort array using insertion sort*/
-    public static void sort(int arr[]) {
-        int n = arr.length;
+    public static void sort(int inpArr[]) {
+        int n = inpArr.length;
         for (int i = 1; i < n; ++i) {
-            int key = arr[i];
+            int key = inpArr[i];
             int j = i - 1;
 
-			/* Move elements of arr[0..i-1], that are
+			/* Move elements of inpArr[0..i-1], that are
 			greater than key, to one position ahead
 			of their current position */
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
+            while (j >= 0 && inpArr[j] > key) {
+                inpArr[j + 1] = inpArr[j];
                 j = j - 1;
             }
-            arr[j + 1] = key;
+            inpArr[j + 1] = key;
         }
     }
 
