c7b0ba90d3e092364d74cec6c2585630b339ec96
Ali Mirzad <97289679+AliMirzad@users.noreply.github.com>
2023-10-23 22:07:10 +0330

    binary search

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,12 @@ public class Main {
 //        System.out.println(_8_PrimeNumber.primeNumber(19));
 //        _9_PrintSquare.printSquare(5);
 //        System.out.println(_13_SingleRepeatElm.singleRepeatElm(new int[]{2, 2, 4, 1, 4, 6, 6, 7, 7}));
-        int[] arr = {5,6,3,1,8,12};
+        int[] arr = {5, 6, 3, 1, 8, 12};
 //        _15_HeapSort.sort(arr);
 //        _15_HeapSort.printArray(arr);
 //        _16_InsertionSort.sort(arr);
 //        _16_InsertionSort.printArray(arr);
+//        int[] myArr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
     }
 }
